7d2d4a4b349b8d867d16f7d875391f3b0b11d090
Oleg <luchkoo22@gmail.com>
2025-10-31 20:22:40 +0200

    finish

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import css from "./App.module.css";
+// import { useState } from "react";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import { useState } from "react";
+import type { Votes, VoteType } from "../../types/votes";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVotes = (type: VoteType) => {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    });
+  };
+
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalVotes: number = votes.good + votes.bad + votes.neutral;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVotes}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={
+            totalVotes ? Math.round((votes.good / totalVotes) * 100) : 0
+          }
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
 }
-
-export default App
